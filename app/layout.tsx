eb225e283dc8402943cf9ac46facefb51312df01
FANUCCI80s <fanuccinicholas@gmail.com>
2026-08-11 03:48:47 -0700

    Add Tawk.to live chat

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
+
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -13,7 +15,33 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+
+        <Script
+          id="tawk-to"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API = Tawk_API || {};
+              var Tawk_LoadStart = new Date();
+
+              (function () {
+                var s1 = document.createElement("script");
+                var s0 = document.getElementsByTagName("script")[0];
+
+                s1.async = true;
+                s1.src = "https://embed.tawk.to/6a7a94b545989d1d4159b0df/1jvnd9q3c";
+                s1.charset = "UTF-8";
+                s1.setAttribute("crossorigin", "*");
+
+                s0.parentNode.insertBefore(s1, s0);
+              })();
+            `,
+          }}
+        />
+      </body>
     </html>
   );
 }
+
